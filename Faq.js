c09686dba0efe8505c580aeac12175dee3e70fb7
biodun-ojo <149972442+biodun-ojo@users.noreply.github.com>
2024-07-20 22:08:55 +0100

    plus img changing done

--- Faq.js
+++ Faq.js
@@ -1,23 +1,35 @@
 const sections = document.getElementsByClassName('secs');
 const show = document.querySelectorAll('p');
+const images = document.getElementsByClassName('plus');
 
-function toggleDisplay(p) {
+function toggleDisplay(p, ge) {
     const currentDisplay = p.style.display;
+
+    const currentimg = ge.src = 'assets/images/icon-plus.svg'
 
     // Hide all paragraphs first
     show.forEach(show => show.style.display = 'none');
 
+    // Reset all images to plus
+    Array.from(images).forEach(img => img.src = 'assets/images/icon-plus.svg');
+
     // Toggle the clicked paragraph's display
-    if (currentDisplay === 'none' || currentDisplay === '') {
+    if ((currentDisplay === 'none' || currentDisplay === '') && (currentimg === 'assets/images/icon-plus.svg')) {
         p.style.display = 'block';
+        ge.src = 'assets/images/icon-minus.svg'
     } else {
         p.style.display = 'none';
+        ge.src = 'assets/images/icon-plus.svg'
     }
+
+    /* if (p.style.display = 'none') {
+        ge.src = 'assets/images/icon-plus.svg'
+    } */
 }
 
 Array.from(sections).forEach((one_of_them, index) => {
     one_of_them.addEventListener('click', () => {
-        toggleDisplay(show[index]);
+        toggleDisplay(show[index], images[index])
+        console.log(show[index], images);
     });
-    console.log(show[index])
 });
